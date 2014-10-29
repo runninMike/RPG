1c237d0a550f58529aff5596068540bb39181e02
runninMike <mike.runyon@ymail.com>
2014-10-28 22:20:06 -0700

    Title Menu
    Made 2 options now.

--- Assets/MainMenu/Scripts/MenuScript.cs
+++ Assets/MainMenu/Scripts/MenuScript.cs
@@ -15,8 +15,20 @@ public class MenuScript : MonoBehaviour
 			(2 * Screen.height / 3) - (buttonHeight / 2),
 			buttonWidth, buttonHeight);
 
+		Rect buttonRect2 = new Rect(
+			Screen.width / 2 - (buttonWidth / 2),
+			(2 * Screen.height / 4) - (buttonHeight / 2),
+			buttonWidth, buttonHeight);
+
 		// Draw button to start the game
-		if (GUI.Button (buttonRect, "Play!"))
+		if (GUI.Button (buttonRect, "Battle!"))
+		{
+			// Load first level on click
+			//Level1 is a test name
+			Application.LoadLevel("BattleTest");
+		}
+
+		if (GUI.Button (buttonRect2, "Town!"))
 		{
 			// Load first level on click
 			//Level1 is a test name
