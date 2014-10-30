7ad1385d4fa47c46650462230e51ab1dfc432c2a
runninMike <mike.runyon@ymail.com>
2014-10-30 11:15:39 -0700

    CHANGES

---
+++ Assets/Collider.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Collider : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnCollisionEnter(Collision col){
+				if (col.gameObject.name == "Door")
+						Debug.Log("A DOOR");
+		}
+}
