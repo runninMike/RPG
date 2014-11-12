58743f1ad86cc391fe823a091a482af1a8f173bd
runninMike <mike.runyon@ymail.com>
2014-11-11 23:11:34 -0800

    Inventory
    Added a scene that tests the inventory, it's in far from working order
    but its the ground work for pretty much anything else I do with the
    inventory.

---
+++ Assets/Important Scripts/Inventory.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Inventory : MonoBehaviour {
+	public List<Item> inventory = new List<Item>();
+	private ItemDatabase database;
+	private bool showInventory = false;
+
+	// Use this for initialization
+	void Start () {
+		database = GameObject.FindGameObjectWithTag ("Item Database").GetComponent<ItemDatabase> (); 
+		inventory.Add (database.items [0]);
+		inventory.Add (database.items [1]);
+		print (inventory[0].itemName); 
+		print (inventory[1].itemName); 
+	}
+
+	void Update(){
+		if (Input.GetKeyDown (KeyCode.I)) {
+			showInventory = !showInventory;
+		}
+	}
+	//the gui for Inventory
+	void OnGUI () {
+		if (showInventory) {
+			DrawInventory();
+		}
+		for (int i = 0; i < inventory.Count; i++)
+		{
+			GUI.Label(new Rect(10, i * 20, 200, 50), inventory[i].itemName);
+		}
+	}
+
+	void DrawInventory(){
+		for (int x = 0; x < 10; x++) {
+			GUI.Box(new Rect(20, x * 20, 20, 20), "Inventory");	
+		}
+	}
+}
---
+++ Assets/Important Scripts/Item.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Item {
+	public string itemName;
+	public int itemID;
+	public string itemDesc;
+	public Texture2D itemIcon;
+	public int itemPower;
+	public ItemType itemType;
+
+	public enum ItemType {
+		Gun,
+		Drink,
+		Mission
+	}
+}
