011ff855827545cfcd3fcf6f17d55f83bb5139a2
runninMike <mike.runyon@ymail.com>
2014-12-01 12:39:01 -0800

    VS plugin
    added visual studios plugin.

--- Assets/Important Scripts/Inventory.cs
+++ Assets/Important Scripts/Inventory.cs
@@ -8,7 +8,8 @@ public class Inventory : MonoBehaviour {
 	private bool showInventory = false;
 
 	// Use this for initialization
-	void Start () {
+	void Start () 
+    {
 		database = GameObject.FindGameObjectWithTag ("Item Database").GetComponent<ItemDatabase> (); 
 		inventory.Add (database.items [0]);
 		inventory.Add (database.items [1]);
@@ -16,25 +17,24 @@ public class Inventory : MonoBehaviour {
 		print (inventory[1].itemName); 
 	}
 
-	void Update(){
-		if (Input.GetKeyDown (KeyCode.I)) {
+	void Update()
+    {
+		if (Input.GetKeyDown (KeyCode.I))
 			showInventory = !showInventory;
-		}
 	}
 	//the gui for Inventory
-	void OnGUI () {
-		if (showInventory) {
+	void OnGUI () 
+    {
+		if (showInventory) 
 			DrawInventory();
-		}
+
 		for (int i = 0; i < inventory.Count; i++)
-		{
 			GUI.Label(new Rect(10, i * 20, 200, 50), inventory[i].itemName);
-		}
 	}
 
-	void DrawInventory(){
-		for (int x = 0; x < 10; x++) {
+	void DrawInventory()
+    {
+		for (int x = 0; x < 10; x++)
 			GUI.Box(new Rect(20, x * 20, 20, 20), "Inventory");	
-		}
 	}
 }
